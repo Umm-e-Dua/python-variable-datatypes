bf4a31c27a246b3b4c6b21827d683ddc65422031
Umm-e-Dua <160483094+Umm-e-Dua@users.noreply.github.com>
2025-02-18 00:33:29 +0500

    Add files via upload

---
+++ indux.py
@@ -0,0 +1,20 @@
+# print hellow word
+print("Hello world")
+
+# data type and variable type check str , num, bool, float , none
+
+name = "Ummedua"
+
+age = 18
+
+my_work = True
+
+final = 3.444
+
+super = None
+
+print(type(name))
+print(type(age))
+print(type(my_work))
+print(type(final))
+print(type(super))
